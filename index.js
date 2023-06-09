37ec32f59a8aad37cda64ebfe4bd3b473a686644
Oyeins-GUI <oyeinx7@gmail.com>
2023-06-09 13:59:34 +0100

    changed conditional for number animation and tried code optimization

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ let startValueMonth = -1;
 let startValueDay = -1;
 let dateHistories;
 
-if (localStorage.getItem("dates") == null) {
-  dateHistories = [];
-} else {
-  dateHistories = JSON.parse(localStorage.getItem("dates"));
-}
-
 const today = new Date();
 const dayInput = document.getElementById("day");
 const monthInput = document.getElementById("month");
@@ -19,13 +13,13 @@ const allInputs = document.querySelectorAll(".date-input");
 const errorDay = document.querySelector(".error-day");
 const errorMonth = document.querySelector(".error-month");
 const errorYear = document.querySelector(".error-year");
-const showResult = document.querySelector(".display-result");
 const yearResult = document.querySelector(".year-result");
 const monthResult = document.querySelector(".month-result");
 const dayResult = document.querySelector(".day-result");
 const yearOutput = document.querySelector(".year-output");
 const monthOutput = document.querySelector(".month-output");
 const dayOutput = document.querySelector(".day-output");
+const showResult = document.querySelector(".display-result");
 const dayText = document.querySelector(".day-text");
 const monthText = document.querySelector(".month-text");
 const yearText = document.querySelector(".year-text");
@@ -33,152 +27,146 @@ const historyMenu = document.querySelector(".history");
 const hamburgerIcon = document.querySelector(".toggle-icon");
 const closeIcon = document.querySelector(".close-history");
 const historyContainer = document.querySelector(".history-content");
-yearInput.dataset.max = today.getFullYear();
+
 yearInput.max = today.getFullYear();
 
-hamburgerIcon.addEventListener("click", () => {
-  historyMenu.classList.add("show");
-});
-closeIcon.addEventListener("click", () => {
-  historyMenu.classList.remove("show");
-});
-showResult.addEventListener("click", displayAge);
-document.addEventListener("keyup", function (e) {
-  if (e.keyCode == 13) {
-    displayAge();
-  }
-});
-
-if (localStorage.length > 0) {
-  getDateAndAddToHistory();
+if (localStorage.getItem("dates") === null) {
+    dateHistories = [];
+} else {
+    dateHistories = JSON.parse(localStorage.getItem("dates"));
 }
 
+function eventListeners() {
+    hamburgerIcon.addEventListener("click", () => {
+        historyMenu.classList.add("show");
+    });
+    closeIcon.addEventListener("click", () => {
+        historyMenu.classList.remove("show");
+    });
+    showResult.addEventListener("click", displayAge);
+    document.addEventListener("keyup", function (e) {
+        if (e.keyCode === 13) {
+            displayAge();
+        }
+    });
+}
+eventListeners();
+
 function getUserInput() {
-  const valueOfDay = Number(dayInput.value);
-  const valueOfMonth = Number(monthInput.value);
-  const valueOfYear = Number(yearInput.value);
+    const valueOfDay = Number(dayInput.value);
+    const valueOfMonth = Number(monthInput.value);
+    const valueOfYear = Number(yearInput.value);
 
-  if (valueOfDay == "" || valueOfMonth == "" || valueOfYear == "") return;
-
-  return { valueOfDay, valueOfMonth, valueOfYear };
+    return { valueOfDay, valueOfMonth, valueOfYear };
 }
 
 function calculateAge() {
-  const { valueOfDay, valueOfMonth, valueOfYear } = getUserInput();
-  let birthDate = new Date(`${valueOfYear}, ${valueOfMonth}, ${valueOfDay}`);
-  let years, months, days;
-  let dateHistory = [];
+    const { valueOfDay, valueOfMonth, valueOfYear } = getUserInput();
+    let birthDate = `${valueOfYear}, ${valueOfMonth}, ${valueOfDay}`;
+    let birthDateObj = new Date(birthDate);
+    let years, months, days;
+    let dateHistory = [];
 
-  // if (localStorage.getItem("dates") == "[]") {
-  //   dateHistories = [];
-  // } else {
-  //   dateHistories = JSON.parse(localStorage.getItem("dates"));
-  // }
+    if (
+        today.getMonth() > birthDateObj.getMonth() ||
+        (today.getMonth() === birthDateObj.getMonth() &&
+            today.getDate() >= birthDateObj.getDate())
+    ) {
+        years = today.getFullYear() - birthDateObj.getFullYear();
+    } else {
+        years = today.getFullYear() - birthDateObj.getFullYear() - 1;
+    }
 
-  if (
-    today.getMonth() > birthDate.getMonth() ||
-    (today.getMonth() == birthDate.getMonth() &&
-      today.getDate() >= birthDate.getDate())
-  ) {
-    years = today.getFullYear() - birthDate.getFullYear();
-  } else {
-    years = today.getFullYear() - birthDate.getFullYear() - 1;
-  }
+    if (today.getDate() >= birthDateObj.getDate()) {
+        months = today.getMonth() - birthDateObj.getMonth();
+    } else if (today.getDate() < birthDateObj.getDate()) {
+        months = today.getMonth() - birthDateObj.getMonth() - 1;
+    }
+    months = months < 0 ? months + 12 : months;
 
-  if (today.getDate() >= birthDate.getDate()) {
-    months = today.getMonth() - birthDate.getMonth();
-  } else if (today.getDate() < birthDate.getDate()) {
-    months = today.getMonth() - birthDate.getMonth() - 1;
-  }
-  months = months < 0 ? months + 12 : months;
+    let monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (today.getDate() >= birthDateObj.getDate()) {
+        days = today.getDate() - birthDateObj.getDate();
+    } else {
+        days =
+            today.getDate() -
+            birthDateObj.getDate() +
+            monthDays[birthDateObj.getMonth()];
+    }
 
-  let monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  if (today.getDate() >= birthDate.getDate()) {
-    days = today.getDate() - birthDate.getDate();
-  } else {
-    days =
-      today.getDate() - birthDate.getDate() + monthDays[birthDate.getMonth()];
-  }
+    dateHistory.push(`${valueOfYear}, ${valueOfMonth}, ${valueOfDay}`);
+    dateHistory.push(`${years}, ${months}, ${days}`);
+    dateHistories.push(dateHistory);
 
-  dateHistory.push(`${valueOfYear}, ${valueOfMonth}, ${valueOfDay}`);
-  dateHistory.push(`${years}, ${months}, ${days}`);
-  dateHistories.push(dateHistory);
+    if (storageAvailable("localStorage")) {
+        localStorage.setItem("dates", JSON.stringify(dateHistories));
+        dateHistory = [];
+    } else {
+        alert("Couldn't add data to history");
+    }
 
-  if (storageAvailable("localStorage")) {
-    localStorage.setItem("dates", JSON.stringify(dateHistories));
-    dateHistory = [];
-  } else {
-    alert("Couldn't add data to history");
-  }
-
-  return { years, months, days };
+    return { years, months, days };
 }
 
 function displayAge() {
-  if (isValid) {
-    const { days, months, years } = calculateAge();
+    if (isValid) {
+        const { days, months, years } = calculateAge();
+        let endValueYear = years;
+        let endValueMonth = months;
+        let endValueDay = days;
 
-    yearResult.dataset.valYear = years;
-    monthResult.dataset.valMonth = months;
-    dayResult.dataset.valDay = days;
+        let durationYear = Math.floor(interval / endValueYear);
+        let durationMonth = Math.floor(interval / endValueMonth);
+        let durationDay = Math.floor(interval / endValueDay);
 
-    let endValueYear = yearResult.dataset.valYear;
-    let endValueMonth = monthResult.dataset.valMonth;
-    let endValueDay = dayResult.dataset.valDay;
+        if (days > 1) {
+            dayText.textContent = "days";
+        } else {
+            dayText.textContent = "day";
+        }
+        if (months > 1) {
+            monthText.textContent = "months";
+        } else {
+            monthText.textContent = "month";
+        }
+        if (years > 1) {
+            yearText.textContent = "years";
+        } else {
+            yearText.textContent = "year";
+        }
 
-    let durationYear = Math.floor(interval / endValueYear);
-    let durationMonth = Math.floor(interval / endValueMonth);
-    let durationDay = Math.floor(interval / endValueDay);
+        let counterYear = setInterval(() => {
+            startValueYear += 1;
+            yearResult.textContent = endValueYear;
+            if (startValueYear === endValueYear) {
+                clearInterval(counterYear);
+                startValueYear = -1;
+            }
+        }, durationYear);
 
-    if (days > 1) {
-      dayText.textContent = "days";
-    } else {
-      dayText.textContent = "day";
+        let counterMonth = setInterval(() => {
+            startValueMonth += 1;
+            monthResult.textContent = endValueMonth;
+            if (startValueMonth === endValueMonth) {
+                clearInterval(counterMonth);
+                startValueMonth = -1;
+            }
+        }, durationMonth);
+
+        let counterDay = setInterval(() => {
+            startValueDay += 1;
+            dayResult.textContent = endValueDay;
+            if (startValueDay === endValueDay) {
+                clearInterval(counterDay);
+                startValueDay = -1;
+            }
+        }, durationDay);
+
+        getDateAndAddToHistory(dateHistories);
     }
-    if (months > 1) {
-      monthText.textContent = "months";
-    } else {
-      monthText.textContent = "month";
-    }
-    if (years > 1) {
-      yearText.textContent = "years";
-    } else {
-      yearText.textContent = "year";
-    }
-
-    let counterYear = setInterval(() => {
-      startValueYear += 1;
-      yearResult.textContent = startValueYear;
-      if (startValueYear == endValueYear) {
-        clearInterval(counterYear);
-        startValueYear = -1;
-      }
-    }, durationYear);
-    let counterMonth = setInterval(() => {
-      startValueMonth += 1;
-      monthResult.textContent = startValueMonth;
-      if (startValueMonth == endValueMonth) {
-        clearInterval(counterMonth);
-        startValueMonth = -1;
-      }
-    }, durationMonth);
-    let counterDay = setInterval(() => {
-      startValueDay += 1;
-      dayResult.textContent = startValueDay;
-      if (startValueDay == endValueDay) {
-        clearInterval(counterDay);
-        startValueDay = -1;
-      }
-    }, durationDay);
-    getDateAndAddToHistory();
-  }
 }
 
-function getDateAndAddToHistory() {
-  let retrievedDate = JSON.parse(localStorage.getItem("dates"));
-  historyContainer.innerHTML = "";
-
-  retrievedDate.forEach((date, index) => {
+function createAndSetHistoryElements(date, index) {
     const prevDate = date[0].split(", ");
     const currentDate = date[1].split(", ");
 
@@ -200,37 +188,44 @@ function getDateAndAddToHistory() {
     li.appendChild(deleteButton);
 
     historyContainer.appendChild(li);
-  });
 }
 
+function getDateAndAddToHistory(histories) {
+    historyContainer.innerHTML = "";
+
+    histories.forEach((date, index) => {
+        createAndSetHistoryElements(date, index);
+    });
+}
+getDateAndAddToHistory(dateHistories);
+
 function deleteHistory(event) {
-  const index = event.target.dataset.index;
+    const index = event.target.dataset.index;
 
-  const historyItem = event.target.parentElement;
-  historyItem.remove();
+    const historyItem = event.target.parentElement;
+    historyItem.remove();
 
-  let retrievedDate = JSON.parse(localStorage.getItem("dates"));
-  retrievedDate.splice(index, 1);
-  localStorage.setItem("dates", JSON.stringify(retrievedDate));
+    dateHistories.splice(index, 1);
+    localStorage.setItem("dates", JSON.stringify(dateHistories));
 }
 
 function storageAvailable(type) {
-  let storage;
-  try {
-    storage = window[type];
-    const x = "__storage_test__";
-    storage.setItem(x, x);
-    storage.removeItem(x);
-    return true;
-  } catch (e) {
-    return (
-      e instanceof DOMException &&
-      (e.code === 22 ||
-        e.code === 1014 ||
-        e.name === "QuotaExceededError" ||
-        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
-      storage &&
-      storage.length !== 0
-    );
-  }
+    let storage;
+    try {
+        storage = window[type];
+        const x = "__storage_test__";
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    } catch (e) {
+        return (
+            e instanceof DOMException &&
+            (e.code === 22 ||
+                e.code === 1014 ||
+                e.name === "QuotaExceededError" ||
+                e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+            storage &&
+            storage.length !== 0
+        );
+    }
 }
